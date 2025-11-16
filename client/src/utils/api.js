81e5b41dd4b12c0e2bdc77813a276dd699901cf6
WaseemS02 <waseemssadi@gmail.com>
2025-11-16 17:02:57 +0200

    baseURL updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 
 toast.error(window.location.href);
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://equity-nest-mt48.vercel.app/api",
     // baseURL: window.location.href + "api",
 
 });
